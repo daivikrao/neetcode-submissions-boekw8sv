d56c01b6ca4ec34e62e10540999bcb5b26e94735
daivikrao <56116492+daivikrao@users.noreply.github.com>
2026-04-21 08:20:36 +0530

    Add: n-queens - submission-3

---
+++ Data Structures & Algorithms/n-queens/submission-3.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    bool isSafe(int row,int col,vector<string>& board,int n){
+        int duprow = row;
+        int dupcol = col;
+
+        while(row >=0 && col >= 0){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+            row -= 1;
+            col -= 1;
+        }
+
+        row = duprow;
+        col = dupcol;
+
+        while(col >= 0){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+            col -= 1;
+        }
+
+        row = duprow;
+        col = dupcol;
+
+        while(row <n && col >= 0){
+            if(board[row][col] == 'Q'){
+                return false;
+            }
+            col -= 1;
+            row += 1;
+        }
+        return true;
+    }
+    void solve(int col,vector<vector<string>>& ans, vector<string>& board,int n,vector<int>& leftRow,vector<int>& upperDiagonal,vector<int>& lowerDiagonal){
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row=0;row<n;row++){
+            if(leftRow[row] == 0 && upperDiagonal[n-1+col-row]==0 && lowerDiagonal[row+col]==0){
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                upperDiagonal[n-1 + col - row] = 1;
+                lowerDiagonal[row + col] = 1;
+                solve(col+1,ans,board,n,leftRow,upperDiagonal,lowerDiagonal);
+                board[row][col] = '.';
+                leftRow[row] = 0;
+                upperDiagonal[n-1 + col - row] = 0;
+                lowerDiagonal[row + col] = 0;
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n,string(n,'.'));
+
+        vector<int> leftRow(n,0);
+        vector<int> upperDiagonal(2*n-1,0);
+        vector<int> lowerDiagonal(2*n-1,0);
+
+        solve(0,ans,board,n,leftRow,upperDiagonal,lowerDiagonal);
+        return ans;
+    }
+};
